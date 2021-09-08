7005ba915397248f67f3e5251ef205ac08081bd1
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-08 14:12:14 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+                                                //Steps:
+    public static void main(String[] args) {    //1
+        // Think Java - Exercise 4.3 (2)
+        System.out.print("No, I ");             //2
+        zoop();                                 //3
+        System.out.print("I ");                 //23
+        baffle();                               //24
+    }                                           //32
+
+        public static void zoop() {             //4
+            baffle();                           //5
+            System.out.print("You wugga ");     //13
+            baffle();                           //14
+    }                                           //22
+
+        public static void baffle() {           //6 //15 //25
+            System.out.print("wug");            //7 //16 //26
+            ping();                             //8 //17 //27
+    }                                           //12 //21 //31
+
+        public static void ping() {             //9 //18 //28
+            System.out.println(".");            //10 //19 //29
+    }                                           //11 //20 //30
+}                                               //33
+
+                                                //Out put er:
+                                                //No, I wug.
+                                                //You wugga wug.
+                                                //I wug.
